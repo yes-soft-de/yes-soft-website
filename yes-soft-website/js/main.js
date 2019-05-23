395178fa9f95dfb526d73e3a91a1098e947d795a
sabeat <sabeat2233@gmail.com>
2019-05-23 04:06:28 +0300

    Add Our Team - Section

--- yes-soft-website/js/main.js
+++ yes-soft-website/js/main.js
@@ -1,3 +1,16 @@
+// idea opacity
+setInterval( function () {
+    const active = $('#ideas li.active');
+    if ( active.is(':last-child') ) {
+        $('#ideas li').eq(0).addClass('active');
+    } else {
+        active.next().addClass('active');
+    }
+    active.removeClass('active');
+}, 2300 );
+
+
+//Projects Carousal
 $('.project-carousal').slick({
     centerMode: true,
     centerPadding: '0',
@@ -15,6 +28,8 @@ $('.project-carousal').slick({
         }
     ]
 });
+
+//Service Carousal
 $('.service-carousal').slick({
     centerMode: true,
     centerPadding: '0',
@@ -32,19 +47,27 @@ $('.service-carousal').slick({
     ]
 });
 
-function ideaOpacity () {
+//Our Team Carousal
+$('.team-carousal').slick({
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000,
+    autoplay: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                arrows: false,
+                slidesToShow: 1
+            }
+        }
+    ]
+});
 
-}
-setInterval( function () {
-    const active = $('#ideas li.active');
-    if ( active.is(':last-child') ) {
-        $('#ideas li').eq(0).addClass('active');
-    } else {
-        active.next().addClass('active');
-    }
-    active.removeClass('active');
-}, 2300 );
 
+// add border to navbar after 86px
 function navbar () {
     const nav = $('.navbar');
     if ($(window).scrollTop() >= 100) {
